e2b09c5c886531ce01c04d6923841bed8f290c95
anishneema <anish.neema2@gmail.com>
2025-06-28 10:54:20 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const basePath = isProd ? '/personalwebsite' : '';
+
 const nextConfig = {
   output: 'export',
+  basePath,
+  assetPrefix: basePath,
 };
 
 export default nextConfig;
